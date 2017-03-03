bba7261ebb3f90c99dd3872cc7553247a98e15b8
Bradley Trowbridge <bradley.trowbridge@yahoo.com>
2017-03-03 14:38:14 -0500

    Purchased new house asset and updated motion sensors

--- Assets/Scripts/Devices/MotionSensor.cs
+++ Assets/Scripts/Devices/MotionSensor.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿
+using System;
 using SmartHomeSystems;
+using UnityEngine;
 
 namespace SmartDevices {
     class MotionSensor : ISmartDevice<SecuritySystem>
@@ -14,7 +13,78 @@ namespace SmartDevices {
             Sphere
         }
 
+        public DetectionShape shape;
         public float range;
+        public float anglePeripheral;
+        private bool isTriggered;
+        private LayerMask detectableLayers = LayerMask.GetMask("Player", "Badguy");
+
+        public void Start()
+        {
+            //initialize detection shapes(?)
+        }
+        void Update()
+        {
+            
+            isTriggered = (isEnabled) ? CheckMotion() : false;
+
+        }
+
+        private bool CheckMotion()
+        {
+            switch (shape)
+            {
+                case DetectionShape.Line:
+                    return LineCast(); 
+                case DetectionShape.Cone:
+                    return ConeCast();
+                case DetectionShape.Sphere:
+
+                    return SphereCast();
+                default:
+                    break;
+            }
+            return false;
+        }
+
+        private bool SphereCast()
+        {
+            var colliders = Physics.OverlapSphere(transform.position, range, detectableLayers);
+            return (colliders.Length > 0) && IsLineOfSite(colliders);
+        }
+
+        private bool IsLineOfSite(Collider[] colliders)
+        {
+            foreach( var c in colliders)
+            {
+                if (Physics.Raycast(transform.position, c.transform.position)) 
+                    return true;
+            }
+            return false;
+        }
+
+        private bool ConeCast()
+        {
+            var colliders = Physics.OverlapSphere(transform.position, range, detectableLayers);
+            return (colliders.Length > 0) && IsLineOfSite(colliders) && IsInView(colliders);
+
+        }
+
+        private bool IsInView(Collider[] colliders)
+        {
+            foreach (var c in colliders)
+            {
+                Vector3 cDirection = c.transform.position - transform.position;
+                if (Vector3.Dot(transform.forward.normalized,cDirection.normalized) <= anglePeripheral)
+                    return true;
+            }
+            return false;
+        }
+
+        private bool LineCast()
+        {
+            return Physics.Raycast(transform.position, transform.forward, range, detectableLayers);
+        }
     }
 
 }
